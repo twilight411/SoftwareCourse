4d474844d3625204cebe7e56793c6fcd39c704fd
雅酸 <15675842+ya-acid@user.noreply.gitee.com>
2026-03-13 14:44:35 +0800

    完成helloworld程序

--- HW1/HelloPrime/Program.cs
+++ HW1/HelloPrime/Program.cs
@@ -1,9 +1,6 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-// 第一个要求：输出“你好！我是xxx”
-Console.WriteLine("你好！我是张三"); // TODO: 把“张三”改成你自己的名字
-
-// 第二个要求：输入上下限，输出区间内所有素数（每行一个）
+// 程序功能：输入上下限，输出区间内所有素数（每行一个）
 Console.Write("请输入上限：");
 int upper = ReadIntFromConsole();
 
---
+++ HW1/HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// 第一个要求：输出“你好！我是xxx”
+Console.WriteLine("你好！我是张三"); // TODO: 把“张三”改成你自己的名字
